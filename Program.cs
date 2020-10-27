a8a0cedbbe53022f8d7ba7412130295cee72acb7
Luiz Augusto Melo <luiz@twistereventos.com>
2020-10-27 20:56:22 -0300

    Created Console Project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LotteryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
